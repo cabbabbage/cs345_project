a7f514dc98ff8be396866b54910d8190bce6835c
cabbabbage <122715492+cabbabbage@users.noreply.github.com>
2024-04-23 19:44:08 -0600

    removing unused modules
    removing unused modules.

--- feature_extraction/Length.py
+++
@@ -1,9 +0,0 @@
-class Length:
-    def __init__(self):
-        self.name = "Length"
-        pass
-
-    def get(self, text):
-        length = len(text.split())
-        print(str(length) + " " + text)
-        return length 
